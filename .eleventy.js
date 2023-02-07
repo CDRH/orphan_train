3daf29914af8d067d85ebd73edb5c8a6a1a81027
TrisCurd <69926455+TrisCurd@users.noreply.github.com>
2023-02-07 13:30:23 -0600

    Finished header and started home page

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = eleventyConfig => {
       });
 
       eleventyConfig.addPassthroughCopy('css')
+      eleventyConfig.addPassthroughCopy('images')
 //used so we can have css files that are copied to the ending website
   return {
     passthroughFileCopy: true
